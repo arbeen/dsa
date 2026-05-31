e38d0d760b04430ac01c0f201cf0b6528a7812be
arbeen <arbinshrestha98@gmail.com>
2026-05-31 10:52:46 -0400

    changes StaticArrays to a utility class

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -2,6 +2,10 @@ package arrays;
 
 public class Arrays{
     public static void main(String[] args){
+		StaticArrays sarr = new StaticArrays(5);
+		sarr.insertEnd(1);
+		sarr.insertAtIndex(4, 4);
+		sarr.printArr();
 		int[] newArr = {1,2};
 		DynamicArrays darr = new DynamicArrays(newArr, 2);
 		darr.printArr();
--- src/arrays/StaticArrays.java
+++ src/arrays/StaticArrays.java
@@ -1,54 +1,59 @@
 package arrays;
 
-public class StaticArrays{
-	public static int[] insertEnd(int[] arr, int length, int capacity, int n){
-		if(length<capacity){
+public class StaticArrays {
+	private int[] arr;
+	private int capacity;
+	private int length;
+
+	StaticArrays(int capacity){
+		this.capacity = capacity;
+		this.arr = new int[capacity];
+		this.length = 0;
+	}
+
+	public int[] insertEnd(int n) {
+		if (length < capacity) {
 			arr[length] = n;
+			length++;
 		}
 		return arr;
 	}
-	
-	public static int[] removeEnd(int[] arr, int length){
-		if(length>0){
-			arr[length-1] = 0;
+
+	public int[] removeEnd() {
+		if (length > 0) {
+			arr[length - 1] = 0;
+			length--;
 		}
 		return arr;
 	}
-	public static void insertAtIndex(int[] arr, int length, int capacity, int n, int pos){
-		if(length<capacity && pos<capacity && pos >= 0){
-			for(int i = length-1; i>=pos; i--){
-				arr[i+1] = arr[i];
+
+	public void insertAtIndex(int n, int pos) {
+		if (length < capacity && pos < capacity && pos >= 0) {
+			for (int i = length - 1; i >= pos; i--) {
+				arr[i + 1] = arr[i];
 			}
 			arr[pos] = n;
+			length++;
 		}
 	}
 
-	public static void removeAtIndex(int[] arr, int length, int pos){
-		if(length>0 && pos>=0 && pos<length){
-			for(int i = pos+1; i<length; i++){
-				arr[i-1] = arr[i];
+	public void removeAtIndex(int pos) {
+		if (length > 0 && pos >= 0 && pos < length) {
+			for (int i = pos + 1; i < length; i++) {
+				arr[i - 1] = arr[i];
 			}
-			arr[length-1] = 0;
+			arr[length - 1] = 0;
+			length--;
 		}
 	}
-	public static void printArr(int[] arr, int length){
-		if(length > 0){
-			for(int i = 0; i<length; i++){
+
+	public void printArr() {
+		if (length > 0) {
+			for (int i = 0; i < capacity; i++) {
 				System.out.print(arr[i] + " ");
 			}
 			System.out.println();
 		}
 	}
-	public static void main(String[] args){
-		int[] arr = new int[10];
-		insertEnd(arr, 0, 10, 1);
-		insertEnd(arr, 1, 10, 2);
-		printArr(arr, 10);
-		insertAtIndex(arr, 2, 10, 69, 0);
-		printArr(arr, 10);
-		removeAtIndex(arr, 3, 0);
-		printArr(arr, 10);
-	}
-		
-}
 
+}
